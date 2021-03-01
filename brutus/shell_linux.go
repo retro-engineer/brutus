1011544c67bbb5493a15cff324575fad8bb0d27e
Peter Toth <globetother@gmail.com>
2021-03-01 13:40:56 +0100

    Delete brutus directory

--- brutus/shell_linux.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// DefaultShell : OS-dependent default shell
-var DefaultShell string = "/bin/sh"
-
-// DefaultArgs : OS-dependent default shell arguments
-var DefaultArgs []string = []string{"-c"}
--- brutus/shell_windows.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// DefaultShell : OS-dependent default shell
-var DefaultShell string = "cmd"
-
-// DefaultArgs : OS-dependent default shell arguments
-var DefaultArgs []string = []string{"/c"}
